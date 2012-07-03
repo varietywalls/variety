c202e44cdec5cd91fceee13fcc554276796de139
Peter Levi <peterlevi@peterlevi.com>
2012-07-03 23:35:09 +0300

    commit before release

--- variety/VarietyWindow.py
+++ variety/VarietyWindow.py
@@ -1,6 +1,6 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
-# Copyright (C) 2012 Peter Levi <peterlevi@peterlevi.com>
+# Peter Levi <peterlevi@peterlevi.com>
 # This program is free software: you can redistribute it and/or modify it 
 # under the terms of the GNU General Public License version 3, as published 
 # by the Free Software Foundation.
