1ee679b144a143a036927a234649dc6b6f3f3e33
Peter Levi <peterlevi@peterlevi.com>
2012-08-03 02:15:23 +0300

    commit before release

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='variety',
-    version='0.4.1',
+    version='0.4.2',
     license='GPL-3',
     author='Peter Levi',
     author_email='peterlevi@peterlevi.com',
