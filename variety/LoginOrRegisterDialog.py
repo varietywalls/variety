8e15bd008ca8b1f8ee46a90e12b31a58876bdd1f
Peter Levi <peterlevi@peterlevi.com>
2014-08-10 16:59:30 +0300

    Smart: More responsive Login/Register buttons

--- variety/LoginOrRegisterDialog.py
+++ variety/LoginOrRegisterDialog.py
@@ -3,8 +3,9 @@
 # This file is in the public domain
 ### END LICENSE
 
-from gi.repository import Gtk  # pylint: disable=E0611
+from gi.repository import Gtk, GObject  # pylint: disable=E0611
 import logging
+import threading
 import urllib2
 from variety.Util import Util
 from variety.Smart import Smart
@@ -45,12 +46,20 @@ class LoginOrRegisterDialog(Gtk.Dialog):
         self.ui = builder.get_ui(self)
 
     def show_login_error(self, msg):
-        self.ui.login_error.set_text(msg)
-        self.ui.login_error.set_visible(True)
+        def _go():
+            self.ui.login_error.set_text(msg)
+            self.ui.login_error.set_visible(True)
+            self.ui.login_spinner.set_visible(False)
+            self.ui.login_spinner.stop()
+        GObject.idle_add(_go)
 
     def show_register_error(self, msg):
-        self.ui.register_error.set_text(msg)
-        self.ui.register_error.set_visible(True)
+        def _go():
+            self.ui.register_error.set_text(msg)
+            self.ui.register_error.set_visible(True)
+            self.ui.register_spinner.set_visible(False)
+            self.ui.register_spinner.stop()
+        GObject.idle_add(_go)
 
     def ajax(self, url, data, error_msg_handler):
         try:
@@ -66,21 +75,32 @@ class LoginOrRegisterDialog(Gtk.Dialog):
             raise
 
     def on_btn_login_clicked(self, widget=None):
-        result = self.ajax(Smart.API_URL + '/login',
-                           {'username': self.ui.login_username.get_text(),
-                            'password': self.ui.login_password.get_text()},
-                           self.show_login_error)
+        self.ui.login_error.set_visible(False)
+        self.ui.login_spinner.set_visible(True)
+        self.ui.login_spinner.start()
 
-        if 'error' in result:
-            self.show_login_error(_(result['error']))
-        else:
-            self.smart.set_user(result)
-            self.destroy()
+        def _go():
+            result = self.ajax(Smart.API_URL + '/login',
+                               {'username': self.ui.login_username.get_text(),
+                                'password': self.ui.login_password.get_text()},
+                               self.show_login_error)
+
+            def _update():
+                if 'error' in result:
+                    self.show_login_error(_(result['error']))
+                else:
+                    self.smart.set_user(result)
+                    self.destroy()
+            GObject.idle_add(_update)
+        threading.Timer(0.1, _go).start()
 
     def on_btn_register_clicked(self, widget=None):
+        self.ui.register_error.set_visible(False)
+        self.ui.register_spinner.set_visible(True)
+        self.ui.register_spinner.start()
+
         if self.ui.register_password.get_text() != self.ui.register_password_confirm.get_text():
-            self.ui.register_error.set_text(_('Passwords must match'))
-            self.ui.register_error.set_visible(True)
+            self.show_register_error(_('Passwords must match'))
             return
 
         user = {'username': self.ui.register_username.get_text(),
@@ -92,14 +112,17 @@ class LoginOrRegisterDialog(Gtk.Dialog):
         if self.smart.user.get("username") is None:
             user.update({'id': self.smart.user['id'], 'authkey': self.smart.user['authkey']})
 
-        result = self.ajax(Smart.API_URL + '/register', user, self.show_register_error)
+        def _go():
+            result = self.ajax(Smart.API_URL + '/register', user, self.show_register_error)
 
-        if 'error' in result:
-            self.ui.register_error.set_text(_(result['error']))
-            self.ui.register_error.set_visible(True)
-        else:
-            self.smart.set_user(result)
-            self.destroy()
+            def _update():
+                if 'error' in result:
+                    self.show_register_error(_(result['error']))
+                else:
+                    self.smart.set_user(result)
+                    self.destroy()
+            GObject.idle_add(_update)
+        threading.Timer(0.1, _go).start()
 
 
 if __name__ == "__main__":
