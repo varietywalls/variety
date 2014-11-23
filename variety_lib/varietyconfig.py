35a7faa7c521e5ea70af5cf395a0ce81abd854df
Peter Levi <peterlevi@peterlevi.com>
2014-11-23 10:31:51 +0200

    Bumping the version number to 0.4.21

--- variety_lib/varietyconfig.py
+++ variety_lib/varietyconfig.py
@@ -29,7 +29,7 @@ __all__ = [
 # files). By default, this is ../data, relative your trunk layout
 __variety_data_directory__ = '../data/'
 __license__ = 'GPL-3'
-__version__ = '0.4.20'
+__version__ = '0.4.21'
 
 import os
 
