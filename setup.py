25e63f5450bb784b3d92e32815e74c09514593f0
Peter Levi <peterlevi@peterlevi.com>
2015-02-08 19:44:09 +0200

    Bumped version ot 0.5.3

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='variety',
-    version='0.5.2',
+    version='0.5.3',
     license='GPL-3',
     author='Peter Levi',
     author_email='peterlevi@peterlevi.com',
--- variety_lib/varietyconfig.py
+++ variety_lib/varietyconfig.py
@@ -29,7 +29,7 @@ __all__ = [
 # files). By default, this is ../data, relative your trunk layout
 __variety_data_directory__ = '../data/'
 __license__ = 'GPL-3'
-__version__ = '0.5.2'
+__version__ = '0.5.3'
 
 import os
 
