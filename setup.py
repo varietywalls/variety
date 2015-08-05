96dfbe4bf42b3f2550fef62703d81170215f525a
Peter Levi <peterlevi@peterlevi.com>
2015-08-05 21:24:39 +0300

    Updated setup.py to not change icon paths in desktop file

--- setup.py
+++ setup.py
@@ -52,31 +52,11 @@ def update_config(values = {}):
     return oldvalues
 
 
-def update_desktop_file(datadir):
-
-    try:
-        fin = file('variety.desktop.in', 'r')
-        fout = file(fin.name + '.new', 'w')
-
-        for line in fin:            
-            if 'Icon=' in line:
-                line = "Icon=%s\n" % (datadir + 'media/variety.svg')
-            fout.write(line)
-        fout.flush()
-        fout.close()
-        fin.close()
-        os.rename(fout.name, fin.name)
-    except (OSError, IOError), e:
-        print ("ERROR: Can't find variety.desktop.in")
-        sys.exit(1)
-
-
 class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
     def run(self):
         values = {'__variety_data_directory__': "'%s'" % (self.prefix + '/share/variety/'),
                   '__version__': "'%s'" % self.distribution.get_version()}
         previous_values = update_config(values)
-        update_desktop_file(self.prefix + '/share/variety/')
         DistUtilsExtra.auto.install_auto.run(self)
         update_config(previous_values)
 
