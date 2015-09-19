1148df5f99c1c784676f54ebbaadd3756916526e
Peter Levi <peterlevi@peterlevi.com>
2015-09-20 00:07:31 +0300

    Bumped the version to 0.5.5

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ class InstallAndUpdateDataDirectory(DistUtilsExtra.auto.install_auto):
 
 DistUtilsExtra.auto.setup(
     name='variety',
-    version='0.5.4',
+    version='0.5.5',
     license='GPL-3',
     author='Peter Levi',
     author_email='peterlevi@peterlevi.com',
