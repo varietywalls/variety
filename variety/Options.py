444fc7df6a813bfcf08391a0dc7cd40d8ace69cf
Peter Levi <peterlevi@peterlevi.com>
2016-10-28 20:11:56 +0300

    Updating the default sources

--- variety/Options.py
+++ variety/Options.py
@@ -650,7 +650,6 @@ class Options:
             [True, Options.SourceType.UNSPLASH, "High-resolution photos from Unsplash.com"],
             [False, Options.SourceType.APOD, "NASA's Astronomy Picture of the Day"],
             [True, Options.SourceType.FLICKR, "user:www.flickr.com/photos/peter-levi/;user_id:93647178@N00;"],
-            [True, Options.SourceType.WALLHAVEN, "nature"],
         ]
 
         self.filters = [
--- variety/Util.py
+++ variety/Util.py
@@ -41,7 +41,7 @@ import platform
 from variety import _u, _str
 
 
-VARIETY_INFO = "Downloaded by Variety wallpaper changer, http://peterlevi.com/variety"
+VARIETY_INFO = ""
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
 
