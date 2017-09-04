4d3766eb745e713ccd270007e521e3da885861e7
James Lu <james@overdrivenetworks.com>
2017-09-04 10:40:34 -0700

    indicator: catch ValueError if gi-appindicator isn't installed
    This fixes the issue mentioned at https://bugs.launchpad.net/variety/+bug/1690763/comments/12
    
    Bug: https://bugs.launchpad.net/variety/+bug/1182671

--- variety/indicator.py
+++ variety/indicator.py
@@ -1,16 +1,16 @@
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
 ### BEGIN LICENSE
 # Copyright (c) 2012, Peter Levi <peterlevi@peterlevi.com>
-# This program is free software: you can redistribute it and/or modify it 
-# under the terms of the GNU General Public License version 3, as published 
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
 # by the Free Software Foundation.
-# 
-# This program is distributed in the hope that it will be useful, but 
-# WITHOUT ANY WARRANTY; without even the implied warranties of 
-# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR 
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
 # PURPOSE.  See the GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License along 
+#
+# You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
 ### END LICENSE
 
@@ -31,7 +31,7 @@ try:
     gi.require_version('AppIndicator3', '0.1')
     from gi.repository import AppIndicator3 # pylint: disable=E0611
     use_appindicator = True
-except ImportError:
+except (ValueError, ImportError):
     use_appindicator = False
 
 from variety_lib import varietyconfig
