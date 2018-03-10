9d5d57e07af073d75b035e9b532ef5c5711eb318
Unit 193 <unit193@ubuntu.com>
2018-03-10 11:04:31 -0800

    Prefer Ayatana indicators if available
    (commiter note: https://ayatanaindicators.github.io/about/ describes some of the reasoning behind this switch)

--- variety/indicator.py
+++ variety/indicator.py
@@ -28,8 +28,12 @@ THEME_ICON_NAME_DARK = "variety-indicator-dark"
 
 try:
     import gi
-    gi.require_version('AppIndicator3', '0.1')
-    from gi.repository import AppIndicator3 # pylint: disable=E0611
+    try:
+        gi.require_version('AyatanaAppIndicator3', '0.1')
+        from gi.repository import AyatanaAppIndicator3 as AppIndicator3 # pylint: disable=E0611
+    except (ValueError, ImportError):
+        gi.require_version('AppIndicator3', '0.1')
+        from gi.repository import AppIndicator3 # pylint: disable=E0611
     use_appindicator = True
 except (ValueError, ImportError):
     use_appindicator = False
