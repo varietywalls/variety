71fc7a319f0008f5bbccb39c745b84f4216a3628
Peter Levi <peterlevi@peterlevi.com>
2019-12-30 16:26:57 +0200

    #112: Add user-created quote plugins for Fortune and UrbanDictionary

---
+++ variety/plugins/builtin/quotes/FortuneSource.py
@@ -0,0 +1,50 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (c) 2019, Dan Jones
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+#
+# Based on gist: https://gist.github.com/goodevilgenius/3878ce0f3e232e3daf5c
+#
+
+import subprocess
+from locale import gettext as _
+
+from variety.plugins.IQuoteSource import IQuoteSource
+
+
+class FortuneSource(IQuoteSource):
+    @classmethod
+    def get_info(cls):
+        return {
+            "name": "Fortunes",
+            "description": _(
+                "Displays quotes using the UNIX fortune program. "
+                "You may want to install additional fortunes packs, e.g. fortunes-bofh-excuses."
+            ),
+            "author": "Dan Jones",
+            "version": "0.1",
+        }
+
+    def get_random(self):
+        fortune = subprocess.check_output(["fortune"]).decode().strip()
+        q = fortune.split("--")
+        quote = q[0].strip()
+        if len(q) > 1:
+            s = q[1].strip()
+            author = s
+        else:
+            author = None
+
+        return [{"quote": quote, "author": author, "sourceName": "Fortune Program", "link": None}]
---
+++ variety/plugins/builtin/quotes/UrbanDictionarySource.py
@@ -0,0 +1,61 @@
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (c) 2019, James Miller
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+from locale import gettext as _
+
+import requests
+
+from variety.plugins.IQuoteSource import IQuoteSource
+
+
+class UrbanDictionarySource(IQuoteSource):
+    @classmethod
+    def get_info(cls):
+        return {
+            "name": "UrbanDictionary Definitions",
+            "description": _("Displays definitions from UrbanDictionary"),
+            "author": "James Miller",
+            "version": "0.1",
+        }
+
+    def get_random(self):
+        dict_dict = requests.get("http://api.urbandictionary.com/v0/random").json()
+
+        def _clean(s):
+            return s.strip().replace("[", "").replace("]", "")
+
+        result = []
+        for entry in dict_dict["list"]:
+            quote = (
+                '"'
+                + entry["word"]
+                + '"'
+                + "\n\n"
+                + _clean(entry["definition"])
+                + "\n\nExample:\n"
+                + _clean(entry["example"].strip())
+            )
+
+            result.append(
+                {
+                    "quote": quote,
+                    "author": entry["author"],
+                    "sourceName": "UrbanDictionary",
+                    "link": entry["permalink"],
+                }
+            )
+
+        return result
