09c925636e8139bcbd9efc5c77ccd4e2eaacbb5a
Peter Levi <peterlevi@peterlevi.com>
2022-06-17 13:01:33 +0300

    Variety 0.8.8

--- variety_lib/varietyconfig.py
+++ variety_lib/varietyconfig.py
@@ -23,7 +23,7 @@ except ImportError:
     # for easy development / running from source.
     __variety_data_directory__ = "../data"
 __license__ = "GPL-3"
-__version__ = "0.8.7"
+__version__ = "0.8.8"
 
 import os
 
