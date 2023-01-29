a0d54a72751ce8f0484d4c5bbf9d636379ba70e4
James Lu <james@overdrivenetworks.com>
2023-01-29 00:54:25 -0800

    Variety 0.8.10

--- variety_lib/varietyconfig.py
+++ variety_lib/varietyconfig.py
@@ -23,7 +23,7 @@ except ImportError:
     # for easy development / running from source.
     __variety_data_directory__ = "../data"
 __license__ = "GPL-3"
-__version__ = "0.8.9"
+__version__ = "0.8.10"
 
 import os
 
